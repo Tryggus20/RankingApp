e78c69704f5bade2e55bd811629917476e328b87
David Campbell <tryggus20@gmail.com>
2024-01-04 11:29:13 -0600

    first commit

---
+++ Controllers/ItemController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace RankingApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    //used command . to use ASP namespace for the below line Controller Base
+    public class ItemController : ControllerBase
+    {
+
+    }
+}
