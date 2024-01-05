96767c56ed7808a64bf7c76e806cae18562bcfb8
David Campbell <tryggus20@gmail.com>
2024-01-05 12:28:19 -0600

    beginning jss front end

---
+++ ClientApp/src/components/RankItems.js
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from 'react';
+
+const RankItems = () => {
+    const [items, setItems] = useState([]);
+ return (
+    <main>
+        (items != null)?<div>
+
+        </div>
+    </main>
+ )   
+}
